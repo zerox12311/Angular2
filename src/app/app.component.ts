42a00ee6b34ec6d1e4aa42a20ca59fb928a8c67b
Lux Protoss <zerox12311@gmail.com>
2017-02-08 13:27:00 +0800

    ADD MENU

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,56 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
 
+declare var SidebarNav: any;
+import '../js/SidebarNav.min.js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [AppService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    // template: `
+    // <ul *ngFor="let key of menu_title">
+    //     {{key}}
+    //     <li *ngFor="let item of menu[key]">
+    //         {{item.menuid}}
+    //     </li>
+    // </ul>
+    // `,
+    styleUrls: ['./app.component.css'],
+    // styleUrls: ['../css/SidebarNav.min.css'],
+    providers: [AppService]
 })
 export class AppComponent {
-  getData: Array<Object> = [];
-  title = 'app works!';
+    getData: Array<{ menu_group: string, link: string, menuid: string }> = [];
+    title = 'app works!';
+    menu: any;
+    menu_title: any;
 
-  constructor( private _appService: AppService ) { }
+    constructor(private _appService: AppService) {
 
-  ngOnInit() {
-      console.log("GET MENU");
+    }
+
+    ngOnInit() {
+        $('.sidebar-menu').SidebarNav();
+        console.log("GET MENU");
         this._appService.getMenu()
             .subscribe(
-                data => this.getData = data,
-                error => console.log("ERROR"),
-                () => {
-                    console.log("FINSHED");
-                    console.log(this.getData);
-                }
+            data => this.getData = data,
+            error => console.log("ERROR"),
+            () => {
+                console.log("FINSHED");
+                console.log(this.getData);
+
+                this.menu = this.getData.reduce(function (pre, cur) {
+                    pre[cur.menu_group] = pre[cur.menu_group] || [];
+                    pre[cur.menu_group].push(cur);
+                    return pre;
+                }, {});
+
+                this.menu_title=Object.keys(this.menu);
+                console.log(Object.keys(this.menu));
+                console.log(this.menu);
+
+            }
             );
-  }
+    }
 }
