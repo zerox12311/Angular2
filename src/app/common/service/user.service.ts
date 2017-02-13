aff66e4cfcc5d547e31c2cbda980ae7ee4171987
Lux Protoss <zerox12311@gmail.com>
2017-02-13 12:01:30 +0800

    修改登入guard

--- src/app/common/service/user.service.ts
+++ src/app/common/service/user.service.ts
@@ -1,46 +1,56 @@
-import {Injectable} from "@angular/core";
+import { Injectable, Input } from "@angular/core";
 
 import { User } from '../models/user.models';
-import {ReplaySubject} from "rxjs";
-import {TokenService} from './token.service';
+import { ReplaySubject } from "rxjs";
+import { TokenService } from './token.service';
+
+
 
 @Injectable()
-export class UserService{
+export class UserService {
   private isAuthenticatedSubject = new ReplaySubject<boolean>(1);
   public isAuthenticated = this.isAuthenticatedSubject.asObservable();
 
-  constructor(private tokenServcie:TokenService){}
+
+  constructor(private tokenServcie: TokenService) { }
+ 
+
 
   //判斷是否登錄
-  populate(){
-    if(this.tokenServcie.getToken()){
-       this.setAuth({username:'xiajinxin',token:'token-1', password:'123'} as User);
+  populate() {
+    if (this.tokenServcie.getToken()) {
+      this.setAuth({ username: 'xiajinxin', token: 'token-1', password: '123' } as User);
     }
-    else{
+    else {
       this.clearAuth();
     }
   }
 
   //設置登陸者
-  setAuth(user : User){
+  setAuth(user: User) {
     this.tokenServcie.saveToekn(user.token);
     this.isAuthenticatedSubject.next(true);
   }
 
   //註銷用戶
-  clearAuth(){
+  clearAuth() {
     this.tokenServcie.destroyToken();
     this.isAuthenticatedSubject.next(false);
   }
 
   //認證用戶是否有登錄過
-  getAuth(user :User):boolean{
-    if(user.username == 'xiajinxin' && user.password == '123'){
-      user.token = 'token-'+new Date;
+  getAuth(user: User): boolean {
+    if (user.username == 'xiajinxin' && user.password == '123') {
+      console.log("LOGIN SECCESS");
+      user.token = 'token-' + new Date;
       this.tokenServcie.saveToekn(user.token);
+      localStorage.setItem('currentUser', "true");
       return true;
     }
+    localStorage.removeItem('currentUser');
     return false;
   }
+
+
 
 }
--- src/app/login/no-auth-guard.service.ts
+++ src/app/login/no-auth-guard.service.ts
@@ -3,32 +3,42 @@
  * Created by xiajinxin on 2017/2/10.
  */
 
-import {Injectable} from "@angular/core";
-import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import {UserService} from '../common/service/user.service';
-import {Observable} from "rxjs";
+import { UserService } from '../common/service/user.service';
+import { Observable } from "rxjs";
 
 @Injectable()
-export class NoAuthGuard implements CanActivate{
-   private result : boolean;
+export class NoAuthGuard implements CanActivate {
+  private result: boolean;
 
-   constructor(private router:Router,
-               private userService:UserService
-   ){}
+  constructor(private router: Router,
+    private userService: UserService
+  ) { }
 
-   canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot):Observable<boolean>|Promise<boolean>|boolean{
-     console.log(this.userService.isAuthenticated);
-     this.userService.isAuthenticated.subscribe(
-       (isAuthenticated)=>{
-         if(!isAuthenticated){
-            this.router.navigate(["/login"]);
-         }
-         this.result=isAuthenticated;
-       }
-     );
-     return this.result;
-   }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    console.log("HIHI");
+    // this.userService.isAuthenticated.subscribe(
+    //   (isAuthenticated) => {
+    //     console.log("OMG" + isAuthenticated);
+    //     if (!isAuthenticated) {
+    //       console.log("PLEASE LOGIN");
+    //       this.router.navigate(["/login"]);
+    //     }
+    //     this.result = isAuthenticated;
+    //   }
+    // );
+
+    if (localStorage.getItem('currentUser')) {
+            // logged in so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page
+        this.router.navigate(['/login']);
+        return false;
+  }
 
 
 
